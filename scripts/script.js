d45666c4781ab1006dbd5921c7e6639d23576812
Jaimehk <122603817+Jaimehk@users.noreply.github.com>
2023-04-24 09:49:48 +0200

    animatieOnClick

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var slider = document.querySelector(".box-slider-1");
+var slideSlider = document.querySelector(".slide");
+var slider2 = document.querySelector(".box-slider-2");
+var slider3 = document.querySelector(".box-slider-3");
+var slider4 = document.querySelector(".box-slider-4");
+var slider5 = document.querySelector(".box-slider-5");
+var slider6 = document.querySelector(".box-slider-6");
+
+// SLIDER 1//
+slider.addEventListener("click", () => 
+slider.classList.toggle("slideSlider"));
+
+slider2.addEventListener("click", () => 
+slider2.classList.toggle("slideSlider2"));
+
+slider3.addEventListener("click", () => 
+slider3.classList.toggle("slideSlider3"));
+
